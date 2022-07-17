53f26ef225df9dde8598e83ffb5a50c3d69416ee
Harley <harley201009@hotmail.com>
2022-07-17 15:42:12 -0300

    Creating mysales project

---
+++ backend/mysales/src/main/java/com/harley/mysales/MysalesApplication.java
@@ -0,0 +1,13 @@
+package com.harley.mysales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysalesApplication.class, args);
+	}
+
+}
---
+++ backend/mysales/src/test/java/com/harley/mysales/MysalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harley.mysales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
